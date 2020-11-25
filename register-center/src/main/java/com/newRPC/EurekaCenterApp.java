f09ea1162595d95cf4cc1e96f54e2afe59846c58
Robirld <421057904@qq.com>
2020-11-26 00:46:02 +0800

    新增module

---
+++ register-center/src/main/java/com/newRPC/EurekaCenterApp.java
@@ -0,0 +1,13 @@
+package com.newRPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaCenterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaCenterApp.class, args);
+    }
+}
