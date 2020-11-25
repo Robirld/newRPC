606916ce1ae3154ca0a5cb298b76aff04e18a3e4
Robirld <421057904@qq.com>
2020-11-26 00:58:35 +0800

    新增module

--- RPC-server/src/main/java/com/newRPC/RpcServer.java
+++ RPC-server/src/main/java/com/newRPC/RpcServer.java
@@ -1,13 +1,21 @@
 package com.newRPC;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class RpcServer {
+public class RpcServer implements ApplicationContextAware {
     public static void main(String[] args) {
         SpringApplication.run(RpcServer.class, args);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
     }
 }
